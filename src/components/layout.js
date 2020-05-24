5d48f1f39084eab9817b55719ee52b46119f0aa2
atsushi iseki <a.iseki1984@gmail.com>
2020-05-24 19:52:30 +0900

    my-gatsby-tutorial-4

---
+++ src/components/layout.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { css } from "@emotion/core"
+import { useStaticQuery, Link, graphql } from "gatsby"
+
+import { rhythm } from "../utils/typography"
+import SEO from "../components/seo"
+
+export default function Layout({ children, title, description }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
+      <SEO title={title} description={description} />
+      <Link to={`/`}>
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
+        >
+          {data.site.siteMetadata.title}
+        </h3>
+      </Link>
+      <Link
+        to={`/about/`}
+        css={css`
+          float: right;
+        `}
+      >
+        About
+      </Link>
+      {children}
+    </div>
+  )
+}
